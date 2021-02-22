c486909d9905a23f26b1a76a57db88a3200bd49a
gnellany <56595263+gnellany@users.noreply.github.com>
2021-02-22 15:14:11 +0000

    Add files via upload

---
+++ download.py
@@ -0,0 +1,15 @@
+import numerapi
+NAPI = numerapi.NumerAPI(verbosity="info")
+
+# Download new data
+DIR = "my_data_directory"
+NAPI.download_current_dataset(dest_path=DIR, unzip=True)
+
+# Load data
+full_path = f'{DIR}/numerai_dataset_{NAPI.get_current_round()}/'
+train = pd.read_csv(full_path + 'numerai_training_data.csv')
+test_df = pd.read_csv(full_path + 'numerai_tournament_data.csv')
+
+# Split validation and test
+val = test_df[test_df['data_type'] == 'validation']
+test = test_df[test_df['data_type'] != 'validation']
